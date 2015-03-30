928e176ef80be0c6d40f4def20913174ce23e050
veliancreate <julian_veling@hotmail.com>
2015-03-30 13:30:22 +0100

    haml initiated

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -5,7 +5,7 @@ feature 'restaurants' do
   feature 'restaurants' do
     context 'no restaurants have been added' do 
       scenario 'should display a prompt to add a restaurant' do 
-        visit '/resaurants'
+        visit '/restaurants'
         expect(page).to have_content 'No restaurants yet'
         expect(page).to have_link 'Add a restaurant'
       end  
