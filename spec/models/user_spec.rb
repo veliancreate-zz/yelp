5c54e0d50a4b7940403ccfd95ce4ba74002cbbbe
veliancreate <julian_veling@hotmail.com>
2015-04-02 14:50:08 +0100

    restaurant reviews through user unit test passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,15 +9,15 @@ describe User, :type => :model do
   it { is_expected.to have_many :reviewed_restaurants}
 
   it 'knows if a user created a restaurant' do 
-    user = User.new(id: 1)
-    restaurant = Restaurant.new(user_id: 1)
+    user = User.create(email: "anothertest@test.com", password: "testtest")
+    restaurant = Restaurant.create(name: "Burger Queen", user: user)
     expect(user.has_created?(restaurant)).to eq(true)  
   end  
 
   it 'knows if a user has reviewed a restaurant' do 
-    user = User.new(id: 1)
-    restaurant = Restaurant.new(user_id: 1)
-    restaurant.reviews = [Review.new({thoughts: "Great!", rating: 5, user_id: 1})]
+    user = User.create(email: "testtesttest@test.com", password: "testtesttest")
+    restaurant = Restaurant.create(name: "MacDonalds", user: user)
+    restaurant.reviews.create(thoughts: "Great!", rating: 5, user: user)
     expect(user.has_reviewed?(restaurant)).to eq(true)
   end  
 
