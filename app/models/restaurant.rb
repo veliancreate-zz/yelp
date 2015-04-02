05b023a111ad0feb4706cc905d5ebf3766f46165
veliancreate <julian_veling@hotmail.com>
2015-04-02 14:25:04 +0100

    added average reviews

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -8,4 +8,9 @@ class Restaurant < ActiveRecord::Base
      self.reviews.create(options = {:thoughts => params[:thoughts], :rating => params[:rating], :user => user} )
   end  
 
+  def average_rating
+    return 'N/A' if reviews.none?
+    4
+  end  
+
 end
