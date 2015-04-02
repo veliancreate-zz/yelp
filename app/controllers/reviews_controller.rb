c49fae8f1a0f2577fd01dec0d756097a7d1ff76d
veliancreate <julian_veling@hotmail.com>
2015-04-02 17:19:36 +0100

    done all v2 tests

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,6 @@
 class ReviewsController < ApplicationController
 
   def new
-    console
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
@@ -12,12 +11,28 @@ class ReviewsController < ApplicationController
       flash[:notice] = 'You can only write one review per restaurant'        
     else
       @restaurant.create_review(review_params, current_user)
-    end  
+    end
+    console  
     redirect_to restaurants_path
   end
 
   def review_params
     params.require(:review).permit(:thoughts, :rating)
+  end  
+
+  def destroy
+    @review = Review.find(params[:id])
+    if current_user
+      if @review.user_id == current_user.id
+        @review.destroy
+        flash[:notice] = "Review deleted"
+      else
+        flash[:notice] = "You cant delete reviews you didnt create"
+      end
+    else
+      flash[:notice] = "You cant delete reviews if you aren't logged in"
+    end    
+    redirect_to restaurants_path
   end  
 
 end
--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -10,14 +10,14 @@ end
 
 feature 'reviewing' do
   
-    before do
-      visit('/')
-      click_link('Sign up')
-      fill_in('Email', with: 'test@example.com')
-      fill_in('Password', with: 'testtest')
-      fill_in('Password confirmation', with: 'testtest')
-      click_button('Sign up')
-    end
+  before do
+    visit('/')
+    click_link('Sign up')
+    fill_in('Email', with: 'test@example.com')
+    fill_in('Password', with: 'testtest')
+    fill_in('Password confirmation', with: 'testtest')
+    click_button('Sign up')
+  end
 
   before {Restaurant.create name: 'KFC'}
 
@@ -27,10 +27,31 @@ feature 'reviewing' do
      fill_in "Thoughts", with: "so so"
      select '3', from: 'Rating'
      click_button 'Leave Review'
-
      expect(current_path).to eq '/restaurants'
      expect(page).to have_content('so so')
   end
+
+  scenario 'does not allow users to delete a review if they arent logged in' do
+    visit '/restaurants'
+    leave_review('So so', '3')
+    click_link('Sign out')
+    click_link('Delete KFC review')
+    expect(page).to have_content("You cant delete reviews if you aren't logged in")
+  end
+
+  scenario 'does not allow users to delete a review if they didnt create it' do
+    visit '/restaurants'
+    leave_review('So so', '3')
+    click_link('Sign out')
+    click_link('Sign up')
+    fill_in('Email', with: 'test2@example.com')
+    fill_in('Password', with: 'test1test1')
+    fill_in('Password confirmation', with: 'test1test1')
+    click_button('Sign up')
+    click_link('Delete KFC review')
+    expect(page).to have_content("You cant delete reviews you didnt create")
+  end
+
 
   scenario 'displays an average rating for all reviews' do
     leave_review('So so', '3')
