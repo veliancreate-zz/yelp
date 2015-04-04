68c6f61ea80319454dc1990a6b491e80decad127
veliancreate <julian_veling@hotmail.com>
2015-04-04 18:55:39 +0100

    created endorsements

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root to: "restaurants#index"
 
-  resources :restaurants do
-    resources :reviews  
+  resources :restaurants, shallow: true do
+    resources :reviews do
+      resources :endorsements
+    end
   end
 
   get '/auth/:facebook/callback', to: 'sessions#create'
